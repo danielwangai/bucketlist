697433294f61222bb7f71c6230f993d6806c13d7
Daniel Wangai <daniel.maina@andela.com>
2017-04-07 17:04:06 +0300

    [Chore] Add more fields to user model

--- models.py
+++ models.py
@@ -10,11 +10,15 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20))
+    last_name = db.Column(db.String(20))
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
 
-    def __init__(self, username, email):
+    def __init__(self, first_name, last_name, username, email):
         self.id = id(self)
+        self.first_name = first_name
+        self.last_name = last_name
         self.username = username
         self.email = email
     
