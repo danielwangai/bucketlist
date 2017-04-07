993cd104241eda5a042474a89a811e5e5fc196a0
Daniel Wangai <daniel.maina@andela.com>
2017-04-07 17:51:54 +0300

    [Feature] Add bucketlist model.

--- models.py
+++ models.py
@@ -21,6 +21,26 @@ class User(db.Model):
         self.last_name = last_name
         self.username = username
         self.email = email
-    
+
     def __repr__(self):
         return '<User %r>' % self.username
+
+class Bucketlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    done = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime)
+    modified_at = db.Column(db.DateTime)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref=db.backref('users', lazy='dynamic'))
+
+    def __init__(self, name, user):
+        self.id = id(self)
+        self.name = name
+        self.done = False
+        self.created_at = datetime.utcnow()
+        self.modified_at = None
+        self.user = user
+    
+    def __repr__(self):
+        return '<Bucketlist %r>' % self.name
