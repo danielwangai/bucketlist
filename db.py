d1f7dd9d8d7faf62708dcd2bdc0316edaa6d522a
Daniel Wangai <daniel.maina@andela.com>
2017-04-10 11:32:00 +0300

    [Chore] Add method to create db

--- db.py
+++ db.py
@@ -1,5 +1,16 @@
+import os.path
+
+from config import SQLALCHEMY_MIGRATE_REPO
 from app import db, manager
 from app.models import User, Bucketlist, Task
+
+@manager.command
+def create_db():
+    if os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+        print("Database already exists.")
+    else:
+        db.create_all()
+        print("Database successfully created.")
 
 if __name__ == '__main__':
     manager.run()
