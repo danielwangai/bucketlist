2da40b0d39ca3a6dc6ebe7cac94435652d4047fb
Daniel Wangai <daniel.maina@andela.com>
2017-04-12 15:33:32 +0300

    [Chore] Add test for single bucketlist fetch.

--- test_api.py
+++ test_api.py
@@ -1,16 +1,25 @@
+"""Tests for API endpoints."""
 import unittest
 # local imports
-from app import db, app
+from app import app
 
 
 class APIEndpointsTestCase(unittest.TestCase):
+    """Test for API endpoints."""
 
     def setUp(self):
+        """Test wide variables."""
         self.app = app.app_context()
         self.client = app.test_client()
 
     def test_fetch_all_bucketlists(self):
+        """Test that endpoint fetches all bucketlists."""
         response = self.client.get("/v1/bucketlists/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_fetch_single_bucketlist(self):
+        """Test that endpoint fetches a single bucketlist."""
+        response = self.client.get('/v1/bucketlists/1/')
         self.assertEqual(response.status_code, 200)
 
 
