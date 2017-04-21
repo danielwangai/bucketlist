21f342e8ff0032601dc800b9f38788ff3b77987c
Daniel Wangai <daniel.maina@andela.com>
2017-04-21 07:12:22 +0300

    [Feature] Add api endpoints

--- run.py
+++ run.py
@@ -2,11 +2,13 @@ import os.path
 
 from flask import Flask
 from flask_migrate import Migrate, MigrateCommand
+from flask_restful import Api
 from flask_script import Manager
 
 from config import configurations  # QLALCHEMY_MIGRATE_REPO
 from app import db
 from app.models import User, Bucketlist, Task
+from app.views import UserLogin, CreateUser, BucketlistResources
 
 
 app = Flask(__name__, instance_relative_config=True)
@@ -14,7 +16,11 @@ app.config.from_object(configurations["development"])
 # add configuration settings from instance/config.py
 app.config.from_pyfile('config.py')
 
+db.init_app(app)
+
 migrate = Migrate(app, db)
+
+api = Api(app)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
@@ -36,5 +42,12 @@ def drop_db():
     db.drop_all()
 
 
+# api endpoints
+api.add_resource(BucketlistResources, "/api/v1/bucketlists",
+                 "/api/v1/bucketlists/<int:id>", endpoint='bucketlist')
+api.add_resource(UserLogin, '/api/v1/auth/login', endpoint="login")
+api.add_resource(CreateUser, '/api/v1/auth/register', endpoint="user_registration")
+
 if __name__ == '__main__':
-    manager.run()
+    # manager.run()
+    app.run(debug=True, host='127.0.0.1', port=5000)
