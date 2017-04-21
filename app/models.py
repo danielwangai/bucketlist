b445e7dfd807499397366f661898e2546f253d44
Daniel Wangai <daniel.maina@andela.com>
2017-04-21 10:58:44 +0300

    [Chore] Remove unnecessary init methods

--- app/models.py
+++ app/models.py
@@ -14,13 +14,6 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
 
-    def __init__(self, first_name, last_name, username, email):
-        self.id = id(self)
-        self.first_name = first_name
-        self.last_name = last_name
-        self.username = username
-        self.email = email
-
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -34,12 +27,6 @@ class Bucketlist(db.Model):
     created_by = db.Column(db.Integer, db.ForeignKey('users.id'))
     items = db.relationship('Item', backref='bucketlist', lazy='dynamic')
 
-    def __init__(self, name):
-        self.id = id(self)
-        self.name = name
-        self.created_at = datetime.utcnow()
-        self.modified_at = None
-
     def __repr__(self):
         return '<Bucketlist %r>' % self.name
 
@@ -52,14 +39,6 @@ class Item(db.Model):
     modified_at = db.Column(db.DateTime)
     done = db.Column(db.Boolean, default=False)
     bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlists.id'))
-
-    def __init__(self, name, bucketlist):
-        self.id = id(self)
-        self.name = name
-        self.done = False
-        self.created_at = datetime.utcnow()
-        self.modified_at = None
-        self.bucketlist = bucketlist
 
     def __repr__(self):
         return '<Task %r>' % self.name
