bbd3504ec4dee696e622fd54e37e983bc2c241f1
Daniel Wangai <daniel.maina@andela.com>
2017-04-21 10:59:44 +0300

    [Chore] Remove unnecessary user fields

--- app/models.py
+++ app/models.py
@@ -9,10 +9,8 @@ from app import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(20))
-    last_name = db.Column(db.String(20))
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    username = db.Column(db.String(32), index=True)
+    password = db.Column(db.String(128))
 
     def __repr__(self):
         return '<User %r>' % self.username
