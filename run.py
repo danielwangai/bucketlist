7b844149bb459218ed9d560c4a44bed4c76e65eb
Daniel Wangai <daniel.maina@andela.com>
2017-04-25 00:19:45 +0300

    [Chore] Add endpoints.

--- run.py
+++ run.py
@@ -8,7 +8,8 @@ from flask_script import Manager
 from config import configurations
 from app import db, create_app, api
 from app.models import User, Bucketlist, Item
-from app.views import UserLogin, CreateUser, BucketlistResources
+from app.views import (UserLogin, CreateUser, BucketlistResources,
+                       BucketlistItemResources)
 
 
 app = create_app("development")
@@ -16,6 +17,10 @@ app = create_app("development")
 # api endpoints
 api.add_resource(BucketlistResources, "/api/v1/bucketlists",
                  "/api/v1/bucketlists/<int:id>", endpoint='bucketlist')
+api.add_resource(BucketlistItemResources,
+                 "/api/v1/bucketlists/<int:bucketlist_id>/items",
+                 "/api/v1/bucketlists/<int:bucketlist_id>/items/<int:item_id>",
+                 endpoint='bucketlist_item')
 api.add_resource(UserLogin, '/api/v1/auth/login', endpoint="login")
 api.add_resource(CreateUser, '/api/v1/auth/register', endpoint="user_registration")
 
