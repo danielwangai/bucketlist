3288cca9a93bf1adb764719fada24d58ac877d4d
Daniel Wangai <daniel.maina@andela.com>
2017-04-24 12:16:44 +0300

    [Chore] Add function to verify auth token.

--- app/views.py
+++ app/views.py
@@ -1,8 +1,22 @@
 from flask import g
+from flask_httpauth import HTTPTokenAuth
 from flask_restful import Resource, reqparse
 
 from app import db
 from app.models import Bucketlist, Item, User
+
+# create instance of HTTPTokenAuth
+auth = HTTPTokenAuth(scheme="Token")
+
+
+@auth.verify_token
+def verify_token(token):
+    """To validate the token sent by the user."""
+    user = User.verify_auth_token(token)
+    if not user:
+        return False
+    g.user = user
+    return True
 
 
 class UserLogin(Resource):
